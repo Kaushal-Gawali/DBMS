cf11d732e338b0ef1bd214d617798ec4ba092301
Kaushal-Gawali <119328448+Kaushal-Gawali@users.noreply.github.com>
2023-05-04 10:56:40 -0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -5,31 +5,36 @@ PRN:- UCS21M1050
 Rno:- 50
 '''
 
-echo "use BRS" > boat_sh.sql
-echo "reserve table"
-echo "select *from sailors;" >> boat_sh.sql
+echo " enter eno | ename | esal | desg | dno | grade "
+read id
+read n
+read s
+read dg
+read dno
+read gr
+echo "use student" > insert.sql
+echo "insert into emp values('$id','$n','$s','$dg','$dno','$gr');">>insert.sql
+echo "Record stored in file"
+echo "select * from emp ">>insert.sql
+sudo mysql -u root < insert.sql
+echo "Done"
 
-echo "bid | sid | dor"
-read bi
-read sid
-read d
-
-echo "insert into reserve values('$bi','sid','d');" >> boat_sh.sql
-echo "select *from sailors;" >> boat_sh.sql
-sudo mysql -u root < boat_sh.sql
-echo "done"
-
-'''
-  reserve table
-bid | sid | dor
-12
-103
-2023-06-05
-sid	sname	srating
-101	xyz	12
-102	abc	15
-103	lmn	19
-[sudo] password for kasuhal:
-done
+use student
+insert into emp values(50,'kaushal','9999','manager','101','A');
+select * from emp
 
 '''
+#OUPUT:
+enter eno | ename | esal | desg | dno | grade 
+50
+kaushal
+9999
+manager
+101
+A
+Record stored in file
+Done
+
+'''
+
+
